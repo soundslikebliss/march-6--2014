c1fb2d540e8e9445e487e1240dd6031297166a56
john cech & David Adams <student@epicodus.com>
2014-03-06 17:03:16 -0800

    last commit of the day

--- calculator_words/lib/calc_words.rb
+++ calculator_words/lib/calc_words.rb
@@ -4,26 +4,24 @@ sentence_array = sentence.split(" ")
 numbers = []
 
 operators = { 
-"plus" => "+",
-"minus" => "-",
-"times" => "*",
-"divided" => "/",
-"power" => "**"
+  "plus" => "+",
+  "minus" => "-",
+  "times" => "*",
+  "divided" => "/",
+  "to" => "**"
 }
 
   sentence_array.each do |x|
     is_number = x.to_f
-      if is_number > 0
-        numbers << is_number
-      elsif !operators[x].nil?
-      numbers << operators[x]
-     end
+    if is_number > 0
+      numbers << is_number
+    elsif !operators[x].nil?
+        numbers << operators[x]
+    end
   end
  
-  
+  # print numbers
   numbers_string = numbers.join(" ")
-  eval(numbers_string)
+  puts eval(numbers_string)
 end  
-# calc_words("what is 5 to the power of 2?")
-
-# what is 2 plus 5 to the 2nd power divided by 4 plus 2
+calc_words("what is 5 to the 2rd power")
--- calculator_words/spec/calc_words_spec.rb
+++ calculator_words/spec/calc_words_spec.rb
@@ -2,22 +2,22 @@ require('rspec')
 require('calc_words')
 
 describe('calc_words') do
- #  it('should add 5 plus 3') do
- #    calc_words("what is 5 plus 3?").should(eq(8))
- #  end
- #  it("should subtract 3 from 5") do
- #    calc_words("what is 5 minus 3?").should(eq(2))
- #  end
- #  it("should multiply 5 by 3") do
- #    calc_words("what is 5 times 3").should(eq(15))
- # end
- #  it("should divide 10 by 2") do
- #    calc_words("what is 10 divided by 2?").should(eq(5))
- #  end
- #  it("should return 5 to the 2nd power") do
- #    calc_words("what is 5 to the power of 2?").should(eq(25))
- #  end
-  it("should be able to calculate equations with multiple opperators") do
+  it('should add 5 plus 3') do
+    calc_words("what is 5 plus 3?").should(eq(8))
+  end
+  it("should subtract 3 from 5") do
+    calc_words("what is 5 minus 3?").should(eq(2))
+  end
+  it("should multiply 5 by 3") do
+    calc_words("what is 5 times 3").should(eq(15))
+ end
+  it("should divide 10 by 2") do
+    calc_words("what is 10 divided by 2?").should(eq(5))
+  end
+  it("should return 5 to the 2nd power") do
+    calc_words("what is 5 to the power of 2?").should(eq(25))
+  end
+  it("should be able to calculate equations with multiple operators") do
     calc_words("what is 5 times 3 divided by 2 plus 5 minus 2").should(eq(10.5))
   end
 end
